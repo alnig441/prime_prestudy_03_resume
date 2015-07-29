243c3c7d86074b66c4a6425bd63d847aa5eecc89
alnig441 <alnig441@gmail.com>
2015-07-29 16:51:29 -0500

    Initial Commit

---
+++ apps.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$('button').on('click', function(){
+		if($('main').css('display')=='none'){
+
+			alert('Press OK to view resume');
+			document.getElementById('button').innerHTML = "Hide Resume";
+			$('main').css({'display': 'inline'});
+			document.getElementById('button').blur();
+
+		}
+		else {
+			alert('Press OK to hide resume');
+			document.getElementById('button').innerHTML = "Show Resume";
+			$('main').css({'display' : 'none'});
+			document.getElementById('button').blur();
+		}
+
+	});
+
+
+});
